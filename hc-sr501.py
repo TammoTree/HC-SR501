232c9fd2e61b27e527174ca7897be9540485da6f
TammoTree <91952536+TammoTree@users.noreply.github.com>
2022-04-29 12:21:16 +0200

    Version 2. Testprogramm funktionsfähig

--- hc-sr501.py
+++ hc-sr501.py
@@ -7,7 +7,7 @@ ldr = Pin(2, Pin.IN)     # create input pin on GPIO2
 
 while True:
 	if ldr.value():
-            print("Detektiert")0
+            print("Detektiert")
         else:
             print("NICHTS Detektiert")
 	time.sleep(1)
